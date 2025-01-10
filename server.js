6ed28e1daabc9336a1c2b9a0b8cc0b5e7d2b7ad2
Amith Krishnan <amithkrishnan20@gmail.com>
2025-01-10 14:08:33 +0530

    server error

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const { scheduleCouponCron } = require('./utils/cronTasks')
 
 const tokenRefresh=require('./Routes/RefreshToken/RefreshRoute')
 const adminAuth=require('./Routes/Admin/Auth/AuthRoute')
-const adminCategory=require('./Routes/Admin/Category/CategouryRoute')
 const userAuth=require('./Routes/User/Auth/AuthRoute')
 const adminCoupon = require('./Routes/Admin/coupon/couponRoute')
 
@@ -24,45 +23,21 @@ app.use('/token',tokenRefresh)
 // admin routes
 
 app.use('/admin/auth', adminAuth)
-<<<<<<< HEAD
-<<<<<<< HEAD
 app.use('/admin/coupon', adminCoupon)
-=======
-=======
-
-app.use('/admin/coupon', adminCoupon)
->>>>>>> 367c68b29fda1c593976f73538097bc49941a24e
 app.use('/admin/category', CategoryRoutes);
 app.use('/admin/subcategory',SubcategoryRoutes);
 
 
-<<<<<<< HEAD
->>>>>>> c45c3fc8db1c664a1324dffc7f91951fe3eda9ab
-=======
->>>>>>> 367c68b29fda1c593976f73538097bc49941a24e
 
 // user routes
 app.use('/user/auth',userAuth)
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-scheduleCouponCron();
-
-
-=======
-
-
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
->>>>>>> c45c3fc8db1c664a1324dffc7f91951fe3eda9ab
-=======
 
 scheduleCouponCron();
 
 
-
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
-
->>>>>>> 367c68b29fda1c593976f73538097bc49941a24e
+ 
 const PORT = 3006 || process.env.PORT
 app.listen(PORT,()=>{
     console.log(`server started listening at PORT ${PORT}`);
